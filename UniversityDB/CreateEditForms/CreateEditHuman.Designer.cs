3549cabe0585ada781fcd9408368b6372bd2ece5
Ulyana Zhovtanetska <ulyana.zhovtanetska@gmail.com>
2017-11-02 01:25:18 +0200

    rename labels

--- UniversityDB/CreateEditForms/CreateEditHuman.Designer.cs
+++ UniversityDB/CreateEditForms/CreateEditHuman.Designer.cs
@@ -28,161 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.name = new System.Windows.Forms.TextBox();
-            this.Surname = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.Title = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.okBtn = new System.Windows.Forms.Button();
-            this.cancelBtn = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            this.tabPage2.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 54);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Surname";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(26, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Age";
-            // 
-            // name
-            // 
-            this.name.Location = new System.Drawing.Point(61, 16);
-            this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(100, 20);
-            this.name.TabIndex = 3;
-            // 
-            // Surname
-            // 
-            this.Surname.Location = new System.Drawing.Point(61, 54);
-            this.Surname.Name = "Surname";
-            this.Surname.Size = new System.Drawing.Size(100, 20);
-            this.Surname.TabIndex = 4;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(61, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
-            // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(13, 12);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 232);
-            this.tabControl1.TabIndex = 6;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.label2);
-            this.tabPage1.Controls.Add(this.Surname);
-            this.tabPage1.Controls.Add(this.label3);
-            this.tabPage1.Controls.Add(this.name);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 206);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Human";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Controls.Add(this.Title);
-            this.tabPage2.Controls.Add(this.textBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 206);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "University object";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // Title
-            // 
-            this.Title.AutoSize = true;
-            this.Title.Location = new System.Drawing.Point(6, 26);
-            this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(27, 13);
-            this.Title.TabIndex = 1;
-            this.Title.Text = "Title";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(64, 23);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // okBtn
-            // 
-            this.okBtn.Location = new System.Drawing.Point(240, 39);
-            this.okBtn.Name = "okBtn";
-            this.okBtn.Size = new System.Drawing.Size(75, 23);
-            this.okBtn.TabIndex = 7;
-            this.okBtn.Text = "Ok";
-            this.okBtn.UseVisualStyleBackColor = true;
-            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
-            // 
-            // cancelBtn
-            // 
-            this.cancelBtn.Location = new System.Drawing.Point(240, 84);
-            this.cancelBtn.Name = "cancelBtn";
-            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
-            this.cancelBtn.TabIndex = 8;
-            this.cancelBtn.Text = "Cancel";
-            this.cancelBtn.UseVisualStyleBackColor = true;
-            // 
-            // CreateEditHuman
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 266);
-            this.Controls.Add(this.cancelBtn);
-            this.Controls.Add(this.okBtn);
-            this.Controls.Add(this.tabControl1);
-            this.Name = "CreateEditHuman";
-            this.Text = "CreateEditHuman";
-            this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabPage2.ResumeLayout(false);
-            this.tabPage2.PerformLayout();
-            this.ResumeLayout(false);
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.name = new System.Windows.Forms.TextBox();
+      this.Surname = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.Title = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.okBtn = new System.Windows.Forms.Button();
+      this.cancelBtn = new System.Windows.Forms.Button();
+      this.tabControl1.SuspendLayout();
+      this.tabPage1.SuspendLayout();
+      this.tabPage2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(6, 16);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "Name";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(6, 54);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(49, 13);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Surname";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(6, 97);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(26, 13);
+      this.label3.TabIndex = 2;
+      this.label3.Text = "Age";
+      // 
+      // name
+      // 
+      this.name.Location = new System.Drawing.Point(61, 16);
+      this.name.Name = "name";
+      this.name.Size = new System.Drawing.Size(100, 20);
+      this.name.TabIndex = 3;
+      // 
+      // Surname
+      // 
+      this.Surname.Location = new System.Drawing.Point(61, 54);
+      this.Surname.Name = "Surname";
+      this.Surname.Size = new System.Drawing.Size(100, 20);
+      this.Surname.TabIndex = 4;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(61, 94);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(100, 20);
+      this.textBox2.TabIndex = 5;
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Location = new System.Drawing.Point(13, 12);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(200, 232);
+      this.tabControl1.TabIndex = 6;
+      // 
+      // tabPage1
+      // 
+      this.tabPage1.Controls.Add(this.label1);
+      this.tabPage1.Controls.Add(this.textBox2);
+      this.tabPage1.Controls.Add(this.label2);
+      this.tabPage1.Controls.Add(this.Surname);
+      this.tabPage1.Controls.Add(this.label3);
+      this.tabPage1.Controls.Add(this.name);
+      this.tabPage1.Location = new System.Drawing.Point(4, 22);
+      this.tabPage1.Name = "tabPage1";
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage1.Size = new System.Drawing.Size(192, 206);
+      this.tabPage1.TabIndex = 0;
+      this.tabPage1.Text = "Human";
+      this.tabPage1.UseVisualStyleBackColor = true;
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.Controls.Add(this.Title);
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(192, 206);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "University object";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      this.tabPage2.Controls.SetChildIndex(this.Title, 0);
+      // 
+      // Title
+      // 
+      this.Title.AutoSize = true;
+      this.Title.Location = new System.Drawing.Point(6, 26);
+      this.Title.Name = "Title";
+      this.Title.Size = new System.Drawing.Size(27, 13);
+      this.Title.TabIndex = 1;
+      this.Title.Text = "Title";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(64, 23);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 0;
+      // 
+      // okBtn
+      // 
+      this.okBtn.Location = new System.Drawing.Point(240, 39);
+      this.okBtn.Name = "okBtn";
+      this.okBtn.Size = new System.Drawing.Size(75, 23);
+      this.okBtn.TabIndex = 7;
+      this.okBtn.Text = "Ok";
+      this.okBtn.UseVisualStyleBackColor = true;
+      this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+      // 
+      // cancelBtn
+      // 
+      this.cancelBtn.Location = new System.Drawing.Point(240, 84);
+      this.cancelBtn.Name = "cancelBtn";
+      this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+      this.cancelBtn.TabIndex = 8;
+      this.cancelBtn.Text = "Cancel";
+      this.cancelBtn.UseVisualStyleBackColor = true;
+      // 
+      // CreateEditHuman
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(589, 377);
+      this.Controls.Add(this.cancelBtn);
+      this.Controls.Add(this.okBtn);
+      this.Controls.Add(this.tabControl1);
+      this.Name = "CreateEditHuman";
+      this.Text = "CreateEditHuman";
+      this.Controls.SetChildIndex(this.tabControl1, 0);
+      this.Controls.SetChildIndex(this.okBtn, 0);
+      this.Controls.SetChildIndex(this.cancelBtn, 0);
+      this.tabControl1.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
+      this.tabPage1.PerformLayout();
+      this.tabPage2.ResumeLayout(false);
+      this.tabPage2.PerformLayout();
+      this.ResumeLayout(false);
 
         }
 
