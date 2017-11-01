d8a54db34dd2c8809bc4f8716d858b4c17d4258f
Iryna Bokalo <iryna.bokalo@eleks.com>
2017-11-02 00:38:31 +0200

    нові зміни....

---
+++ UniversityDB/Select.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversityDB
+{
+    public partial class Select : Form
+    {
+        public string SelectedType;
+
+        public Select(List<string> types)
+        {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(types.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SelectedType = comboBox1.SelectedItem.ToString();
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
